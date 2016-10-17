be684994cb8ed039569989ef0347c1c41f3c998a
Christoph Heindl <christoph.heindl@gmail.com>
2016-10-17 08:39:40 +0200

    Adding STDIN source

--- tonedetect/sources.py
+++ tonedetect/sources.py
@@ -2,6 +2,7 @@
 import subprocess as sp
 import numpy as np
 from tonedetect import helpers
+from sys import stdin
 
 
 class FFMPEGSource(object):  # pylint: disable=too-few-public-methods
@@ -27,9 +28,22 @@ class FFMPEGSource(object):  # pylint: disable=too-few-public-methods
                 break
             audio = np.fromstring(data, dtype="int16")
             yield helpers.normalize_audio_by_bit_depth(audio)
+
+class STDINSource(object):
+
+    @staticmethod
+    def generate_parts(part_length=1024, stype="int16"):
+        while True:
+            data = stdin.read(part_length)
+            if not data:
+                break
+            audio = np.fromstring(data, stype)
+            yield helpers.normalize_audio_by_bit_depth(audio)
             
 class SilenceSource(object):
+    """Generates silence for a desired duration. Useful to flush pending detector results once real input has ended."""
     
     @staticmethod
     def generate_parts(duration, sample_rate=44100):
         yield np.zeros(int(duration * sample_rate))
+
