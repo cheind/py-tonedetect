64946a2b9ee452c1f11bc205f8f219a98d5a3137
Christoph Heindl <christoph.heindl@gmail.com>
2016-10-17 17:58:43 +0200

    Adapting to new sources structure

--- examples/pipeline.py
+++ examples/pipeline.py
@@ -10,19 +10,18 @@ from tonedetect.window import Window
 
 class DefaultDetectionPipeline:
 
-    def __init__(self, gen_parts, sample_rate, tones):
+    def __init__(self, source, tones, min_tone_amp=0.1, max_inter_tone_amp=0.1, min_presence=0.04, min_pause=0.04, max_tone_interval=1, min_sequence_length=1):
         self.tones = tones
         self.freqs = tones.all_tone_frequencies()
 
-        self.wnd = Window.tuned(sample_rate, self.freqs, power_of_2=True)
+        self.wnd = Window.tuned(source.sample_rate, self.freqs, power_of_2=True)
 
         self.d_f = detectors.FrequencyDetector(self.freqs)
-        self.d_t = detectors.ToneDetector(tones, min_tone_amp=0.1, max_inter_tone_amp=0.1, min_presence=0.04, min_pause=0.04)
-        self.d_s = detectors.ToneSequenceDetector(max_tone_interval=1, min_sequence_length=1)
+        self.d_t = detectors.ToneDetector(tones, min_tone_amp=min_tone_amp, max_inter_tone_amp=max_inter_tone_amp, min_presence=min_presence, min_pause=min_pause)
+        self.d_s = detectors.ToneSequenceDetector(max_tone_interval=max_tone_interval, min_sequence_length=min_sequence_length)
 
-        silence = SilenceSource()
-        gen_silence = silence.generate_parts(1, sample_rate)
-        self.gen_windows = self.wnd.update(itertools.chain(gen_parts, gen_silence))
+        silence = SilenceSource(max_tone_interval*2, source.sample_rate)
+        self.gen_windows = self.wnd.update(itertools.chain(source.generate_parts(), silence.generate_parts()))
 
     def read(self):
         yield from self.gen_windows
